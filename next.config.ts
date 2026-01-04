a53da6b3d6b80866b39585e9dee76b4ee39832be
qwerty2944 <qwerty2944@users.noreply.github.com>
2026-01-04 15:46:05 +0900

    feat: 레이아웃 설정 (모바일 dvh, safe-area)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,49 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/unity/:path*.br",
+        headers: [
+          {
+            key: "Content-Encoding",
+            value: "br",
+          },
+          {
+            key: "Content-Type",
+            value: "application/javascript",
+          },
+        ],
+      },
+      {
+        source: "/unity/:path*.wasm.br",
+        headers: [
+          {
+            key: "Content-Encoding",
+            value: "br",
+          },
+          {
+            key: "Content-Type",
+            value: "application/wasm",
+          },
+        ],
+      },
+      {
+        source: "/unity/:path*.data.br",
+        headers: [
+          {
+            key: "Content-Encoding",
+            value: "br",
+          },
+          {
+            key: "Content-Type",
+            value: "application/octet-stream",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
